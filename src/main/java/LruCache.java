08fb32e1889da0d80f438b86fcb8b392a9cd8dc3
Dan Creswell <dan.creswell@gmail.com>
2018-11-03 18:23:07 +0000

    Minor code cleanup for LRU Cache

--- src/main/java/LruCache.java
+++ src/main/java/LruCache.java
@@ -3,16 +3,16 @@ import java.util.LinkedHashMap;
 public class LruCache<K,V> extends LinkedHashMap<K,V> {
 
     private static final long serialVersionUID = 1L;
-    private final int cacheSize;
+    private final int _cacheSize;
 
     public LruCache(int size) {
         super(size, 0.75f, true);
-        this.cacheSize = size;
+        this._cacheSize = size;
 
     }
 
     @Override
     protected boolean removeEldestEntry(java.util.Map.Entry<K,V> eldest) {
-        return size() > cacheSize;
+        return size() > _cacheSize;
     }
 }
