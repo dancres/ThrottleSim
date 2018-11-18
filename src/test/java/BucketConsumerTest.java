cc2e826ead76ea2709c091576e8af8d230fe303b
Dan Creswell <dan.creswell@gmail.com>
2018-11-18 08:50:43 +0000

    Couple of unchecked suppressions

--- src/test/java/BucketConsumerTest.java
+++ src/test/java/BucketConsumerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class BucketConsumerTest {
     @Test
     public void test() {
+        @SuppressWarnings("unchecked")
         Bucket<Integer>[] myBuckets = new Bucket[2];
         myBuckets[0] = new FixedDurationBucket(50, 2000);
         myBuckets[1] = new FixedDurationBucket(100, 2000);
@@ -29,6 +30,7 @@ public class BucketConsumerTest {
 
     @Test
     public void timeCeilingTest() {
+        @SuppressWarnings("unchecked")
         Bucket<Integer>[] myBuckets = new Bucket[1];
         myBuckets[0] = new TimeCeilingBucket(100, 100.0, 2000);
         BucketConsumer<Integer> myConsumer = new BucketConsumer<>(myBuckets, new Well44497b());
